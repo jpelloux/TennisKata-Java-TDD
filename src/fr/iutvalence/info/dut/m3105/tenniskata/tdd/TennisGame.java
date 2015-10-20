f5bfc236a75a1f4e9528bb1344d1bdb61cc5627f
pellouju <pellouju@PC-B124-03.iut-valence.fr>
2015-10-20 16:59:20 +0200

    iter1 green

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -1,9 +1,19 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
-
+	private String score;
+	
+	public TennisGame()
+	{
+		this.score = "love-all";
+	}
+	
 	public String getScore() {
-		return "love-all";
+		return this.score;
+	}
+
+	public void servingPlayerMarks() {
+		this.score = "fifteen-love";
 	}
 
 }
