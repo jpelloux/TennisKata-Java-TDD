3024e317926d1583a03267e461b2e854fed9e071
pellouju <pellouju@PC-B124-03.iut-valence.fr>
2015-10-20 18:11:30 +0200

    iter4 red

--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -9,10 +9,13 @@ import org.junit.Test;
 
 public class TennisGameTest
 {
-	public static final String LOVE_FIFTEEN = "love-fifteen";
-	public static final String FIFTEEN_LOVE = "fifteen-love";
-	public static final String LOVE_ALL = "love-all";
-	public static final String FIFTEEN_ALL = "fifteen-all";
+	public static final String[][] scoreArray =
+		{
+			{"love-all"    , "love-fifteen"  , "love-thirty"   , "love-forty"   },
+			{"fifteen-love", "fifteen-all"   , "fifteen-thirty", "fifteen-forty"},
+			{"thirty-love" , "thirty-fifteen", "thirty-all"    , "thirty-forty" },
+			{"forty-love"  , "forty-fifteen" , "forty-thirty"  , "deuce"        },
+		};
 
 	private TennisGame gameDummy;
 	
@@ -25,21 +28,21 @@ public class TennisGameTest
 	@Test
 	public void aNewGameScoreShouldBeLoveAll()
 	{
-		assertEquals(gameDummy.getScore(), LOVE_ALL);
+		assertEquals(gameDummy.getScore(), scoreArray[0][0]);
 	}
 
 	@Test
 	public void ifTheServerScoresScoreShouldBeFifteenLove()
 	{
 		gameDummy.serverScores();
-		assertEquals(gameDummy.getScore(), FIFTEEN_LOVE);
+		assertEquals(gameDummy.getScore(), scoreArray[1][0]);
 	}
 	
 	@Test
 	public void ifTheReceiverScoresScoreShouldBeLoveFifteen()
 	{
 		gameDummy.receiverScores();
-		assertEquals(gameDummy.getScore(), LOVE_FIFTEEN);
+		assertEquals(gameDummy.getScore(), scoreArray[0][1]);
 	}
 	
 	@Test
@@ -47,11 +50,41 @@ public class TennisGameTest
 	{
 		gameDummy.serverScores();
 		gameDummy.receiverScores();
-		assertEquals(gameDummy.getScore(), FIFTEEN_ALL);
+		assertEquals(gameDummy.getScore(), scoreArray[1][1]);
 		this.before();
 		gameDummy.receiverScores();
 		gameDummy.serverScores();
-		assertEquals(gameDummy.getScore(), FIFTEEN_ALL);
+		assertEquals(gameDummy.getScore(), scoreArray[1][1]);
+	}
+	
+	@Test
+	public void allScoresBeforeEqualityShouldWork()
+	{
+		for (int serverScore = 0 ; serverScore < 4 ; serverScore++)
+		{
+			for (int receiverScore = 0 ; receiverScore < 4 ; receiverScore++)
+			{
+				before();
+				this.setScores(serverScore, receiverScore);
+				assertEquals(gameDummy.getScore(), scoreArray[serverScore][receiverScore]);
+			}
+		}
+	}
+	
+	
+	
+	private void setScores(int serverScore, int receiverScore)
+	{
+		for (int i = 0 ; i < serverScore ; i++)
+		{
+			gameDummy.serverScores();
+		}
+		
+		for (int i = 0 ; i < receiverScore ; i++)
+		{
+			gameDummy.receiverScores();
+		}
+			
 	}
 	
 
